75642498e22d8255f4ec11dd881b54894daad225
Ben Paran Rutterford <ben.p.rutterford@gmail.com>
2022-08-28 22:21:43 +0100

    Refactor functions

--- src/cli.ts
+++ src/cli.ts
@@ -5,32 +5,40 @@ import * as weaknesses from "../data/weaknesses.json" assert { type: "json" };
 
 const P = new Pokedex();
 
-const fetchPokemonInfo = async (name: string) => {
+const getPokemon = async (name: string) => {
   console.log(chalk.green(`Fetching information about ${name}...`));
   try {
     const pokemon = await P.getPokemonByName(name.toLowerCase()) as Pokedex.Pokemon;
-    const pokemonTypes = pokemon.types.map((typeObject) => typeObject.type.name);
-    return pokemonTypes;
+    return pokemon;
   } catch (error) { 
     console.log(chalk.bgRedBright("ERROR - Pokemon not found"));
   }
 };
 
-const fetchPokemonWeakness = () => {
-  }
+const getPokemonTypes = (pokemon: Pokedex.Pokemon) =>
+  pokemon.types.map((typeObject) => typeObject.type.name);
+  
+const getPokemonWeakness = (types: string[]) => {
+
+};
 
 export const cli = async (args: string[]) => {
   console.log(chalk.blue("Welcome to poke-info!"));
+  
   const pokemonName = args[2];
   if (!pokemonName) {
     console.log(chalk.red("Please provide a Pokemon name."));
     exit(1);
   }
-  const pokemonInfo = await fetchPokemonInfo(pokemonName);
-  if (!pokemonInfo) {
-    exit(1)
-  }
-  console.log(pokemonInfo);
+  
+  const pokemonInfo = await getPokemon(pokemonName);
+  if (!pokemonInfo) exit(1);
+  
+  const pokemonTypes = getPokemonTypes(pokemonInfo);
+  console.log(`This Pokemon is of type ${pokemonTypes.join(" and ")}`);
+  
+  const pokemonWeakness = getPokemonWeakness(pokemonTypes);
+  console.log(`This Pokemon's weaknesses are ${pokemonWeakness}`);
 };
 
 cli(argv);
